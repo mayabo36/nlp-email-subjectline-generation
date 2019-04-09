0d99ef3dd366b3fafc4a906444b137e339e9cca2
Melissa Gramajo <melissa.gramajo@servicenow.com>
2019-04-09 16:36:32 -0400

    pushing new subject line generator that follows a bottom up pattern

---
+++ Generator/bottom_up_parser.py
@@ -0,0 +1,73 @@
+import nltk
+from nltk.tokenize import sent_tokenize,word_tokenize
+from nltk.corpus import stopwords
+from collections import defaultdict
+from string import punctuation
+from heapq import nlargest
+
+class FrequencySummarizer:
+  def __init__(self, min_cut=0.1, max_cut=0.9):
+    """
+     Initilize the text summarizer.
+     Words that have a frequency term lower than min_cut 
+     or higer than max_cut will be ignored.
+    """
+    self._min_cut = min_cut
+    self._max_cut = max_cut 
+    self._stopwords = set(stopwords.words('english') + list(punctuation))
+
+  def _compute_frequencies(self, word_sent):
+    """ 
+      Compute the frequency of each of word.
+      Input: 
+       word_sent, a list of sentences already tokenized.
+      Output: 
+       freq, a dictionary where freq[w] is the frequency of w.
+    """
+    freq = defaultdict(int)
+    for s in word_sent:
+      for word in s:
+        if word not in self._stopwords:
+          freq[word] += 1
+    # frequencies normalization and fitering
+    m = float(max(freq.values()))
+    for w in freq.keys():
+      freq[w] = freq[w]/m
+      if freq[w] >= self._max_cut or freq[w] <= self._min_cut:
+        del freq[w]
+    return freq
+
+  def summarize(self, text, n):
+    """
+      Return a list of n sentences 
+      which represent the summary of text.
+    """
+    sents = sent_tokenize(text)
+    assert n <= len(sents)
+    word_sent = [word_tokenize(s.lower()) for s in sents]
+    self._freq = self._compute_frequencies(word_sent)
+    ranking = defaultdict(int)
+    for i,sent in enumerate(word_sent):
+      for w in sent:
+        if w in self._freq:
+          ranking[i] += self._freq[w]
+    sents_idx = self._rank(ranking, n)    
+    return [sents[j] for j in sents_idx]
+
+  def _rank(self, ranking, n):
+    """ return the first n sentences with highest ranking """
+    return nlargest(n, ranking, key=ranking.get)
+
+#main
+
+body = ["Im really interested to hear more about your role as a Manager at Google. If you have time in the coming weeks lets chat. Im generally free on Friday, if that works for you? Looking forward to keeping in touch!", "Hi Bob, It was nice to meet you at the job fair. I would love to learn more about management.", ]
+
+fs = FrequencySummarizer()
+#for article_url in to_summarize[:5]:
+title, text = body
+print '----------------------------------'
+print text
+for s in fs.summarize(text, 2):
+    print '*',s
+for s in fs.summarize(title, 2):
+    print '*',s
